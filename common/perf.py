1332f0e72cfc80e15a68495e8a488d326a9e7fdc
Kevin Su <kevinsoup@gmail.com>
2023-12-22 12:30:15 -0500

    formatting

--- common/perf.py
+++ common/perf.py
@@ -1,10 +1,10 @@
 from time import perf_counter
 
 def profiler(method):
-    def wrapper_method(*arg, **kw):
-        t = perf_counter()
-        ret = method(*arg, **kw)
-        print("Method " + method.__name__ + " took : " + "{:2.5f}".format(perf_counter() - t) + " sec")
-        return ret
+  def wrapper_method(*arg, **kw):
+    t = perf_counter()
+    ret = method(*arg, **kw)
+    print("Method " + method.__name__ + " took : " + "{:2.5f}".format(perf_counter() - t) + " sec")
+    return ret
 
-    return wrapper_method
+  return wrapper_method
