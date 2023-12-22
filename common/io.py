8f8f6e5be861024dabaf69b7472e0133eeec9089
Kevin Su <kevinsoup@gmail.com>
2023-12-22 11:33:38 -0500

    update common files with a test template

--- common/io.py
+++ common/io.py
@@ -3,6 +3,10 @@ def pretty_print(array):
     print(' '.join(list(map(str, row))))
   print()
 
-def get_2d_int_array_from_file(input_file):
+def get_2d_int_input(input_file):
   file = open(input_file, 'r')
   return [[int(c) for c in line.strip()] for line in file.readlines()]
+
+def get_2d_string_input(input_file):
+  file = open(input_file, 'r')
+  return [[c for c in line.strip()] for line in file.readlines()]
---
+++ common/test.py
@@ -0,0 +1,8 @@
+import unittest
+
+class Test(unittest.TestCase):
+  def test_something(self):
+    self.assertTrue(False)
+
+if __name__ == '__main__':
+  unittest.main()
