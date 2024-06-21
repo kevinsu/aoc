56afa93759ff36b33a0e08e92db65a00335c586c
Kevin Su <kevinsoup@gmail.com>
2024-06-21 09:41:44 -0400

    2016 day 17

--- 2016/day17/part2.py
+++ 2016/day17/part2.py
@@ -18,35 +18,34 @@ class Directions:
         return f"up: {self.up}, down: {self.down}, left: {self.left}, right: {self.right}"
 
 
+max_length = 0
+
+
 @profiler
 def main(argv):
     input_file = argv[0]
     file = open(input_file, 'r')
     passcode = file.readline()
-    path = bfs(passcode)
-    print(path)
-    print(len(path))
+    dfs(passcode, 0, 0, '')
+    print(max_length)
 
-def bfs(passcode):
-    q = [(0, 0, '')]
-    while q:
-        current = q.pop(0)
-        x = current[0]
-        y = current[1]
-        path = current[2]
-        if x == 3 and y == 3:
-            return current[2]
-        directions = get_directions(passcode, path)
-        if directions.up and y > 0:
-            q.append((x, y-1, path+'U'))
-        if directions.down and y < 3:
-            q.append((x, y+1, path+'D'))
-        if directions.left and x > 0:
-            q.append((x-1, y, path+'L'))
-        if directions.right and x < 3:
-            q.append((x+1, y, path+'R'))
-        
-        
+
+def dfs(passcode, x, y, path):
+    if x == 3 and y == 3:        
+        global max_length    
+        max_length = max(len(path), max_length)
+        return
+    directions = get_directions(passcode, path)
+    if directions.up and y > 0:
+        dfs(passcode, x, y-1, path+'U')
+    if directions.down and y < 3:
+        dfs(passcode, x, y+1, path+'D')
+    if directions.left and x > 0:
+        dfs(passcode, x-1, y, path+'L')
+    if directions.right and x < 3:
+        dfs(passcode, x+1, y, path+'R')
+
+
 def get_directions(passcode, path):
     result = hashlib.md5((passcode+path).encode()).hexdigest()
     directions = Directions()
