a7a5fe4cbb2be2e9d4add4d27a61babb37d357e0
Kevin Su <kevinsoup@gmail.com>
2024-12-21 11:37:12 -0500

    day 20 and 21

--- 2024/day20/main.py
+++ 2024/day20/main.py
@@ -1,12 +1,111 @@
+import heapq
 import sys
 from common.perf import profiler
+from common.io import get_2d_string_input as get_input
+
+
+def get_start_end(grid):
+    sx = None
+    sy = None
+    ex = None
+    ey = None
+    for i in range(0, len(grid)):
+        for j in range(0, len(grid[0])):
+            if grid[i][j] == 'S':
+                sx = i
+                sy = j
+            elif grid[i][j] == 'E':
+                ex = i
+                ey = j
+    return sx, sy, ex, ey
+
+
+def get_neighbors(grid, x, y):
+    directions = {(0, 1), (0, -1), (1, 0), (-1, 0)}
+    for dx, dy in directions:
+        nx = x + dx
+        ny = y + dy
+        if nx < 0 or nx >= len(grid):
+            continue
+        if ny < 0 or ny >= len(grid[0]):
+            continue
+        if grid[nx][ny] in ('.', 'E', 'S'):
+            yield nx, ny
+
+
+def get_cheat_neighbors(grid, x, y, limit=2):
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] not in ('.', 'E'):
+                continue
+            manhattan = abs(x-i) + abs(y-j)
+            if manhattan < 2:
+                continue
+            if manhattan > limit:
+                continue
+            yield i, j, manhattan
+    
+def get_shortest_path_with_map(grid, ex, ey):
+    candidates = []
+    heapq.heappush(candidates, (0, ex, ey))
+    visited = set()
+    distance_map = {}
+    while candidates:
+        distance, x, y = heapq.heappop(candidates)
+        if distance_map.get((x, y), sys.maxsize) > distance:
+            distance_map[(x, y)] = distance
+        if (x, y) in visited:
+            continue
+        visited.add((x, y))
+        for nx, ny in get_neighbors(grid, x, y):
+            heapq.heappush(candidates, (distance+1, nx, ny))
+    return distance_map
+
+def count_cheats(grid, sx, sy, ex, ey, savings, limit=2):
+    candidates = [(sx, sy, 0, {(sx, sy)})]
+    visited = set()
+    sum = 0
+    all_shortest_paths = get_shortest_path_with_map(grid, ex, ey)
+    max_distance = all_shortest_paths[(sx, sy)]    
+    target = max_distance - savings    
+    while candidates:
+        x, y, distance, path = candidates.pop(0)        
+        if (x, y) in visited:
+            continue
+        if distance > target:
+            continue
+        visited.add((x, y))
+        if (x, y) == (ex, ey):
+            sum += 1
+            continue
+        for nx, ny in get_neighbors(grid, x, y):
+            if (nx, ny) in path:
+                continue
+            if (nx, ny) in visited:
+              continue
+            candidates.append((nx, ny, distance+1, path | {(nx, ny)}))        
+        for nx, ny, nd in get_cheat_neighbors(grid, x, y, limit=limit):
+            if (nx, ny) in path:
+              continue
+            shortest_path = all_shortest_paths[(nx, ny)]
+            new_distance = distance + nd + shortest_path
+            if new_distance <= target:
+                sum += 1   
+    return sum
+
+
+def part1(grid):
+    sx, sy, ex, ey = get_start_end(grid)    
+    return count_cheats(grid, sx, sy, ex, ey, 100, limit=20)
+
 
 @profiler
 def main(argv):
-  input_file = argv[0]
-  file = open(input_file, 'r')
-  for line in file.readlines():
-    print(line)
+    grid = get_input(argv[0])
+    sx, sy, ex, ey = get_start_end(grid)    
+    print(count_cheats(grid, sx, sy, ex, ey, 100, limit=2))
+    print(count_cheats(grid, sx, sy, ex, ey, 100, limit=20))
+    
 
 if __name__ == "__main__":
-  main(sys.argv[1:])
+    main(sys.argv[1:])
--- 2024/day21/main.py
+++ 2024/day21/main.py
@@ -1,12 +1,130 @@
+import itertools
 import sys
 from common.perf import profiler
 
+NUM_PAD = {
+    '7': (0, 0),
+    '8': (0, 1),
+    '9': (0, 2),
+    '4': (1, 0),
+    '5': (1, 1),
+    '6': (1, 2),
+    '1': (2, 0),
+    '2': (2, 1),
+    '3': (2, 2),
+    '0': (3, 1),
+    'A': (3, 2)
+}
+
+DIR_PAD = {
+    '^': (0, 1),
+    'A': (0, 2),
+    '<': (1, 0),
+    'v': (1, 1),
+    '>': (1, 2)
+}
+
+def helper(sx, sy, ex, ey, is_num_pad=True):    
+    if sx < 0:
+        return set()
+    if sy < 0:
+        return set()
+    if sx == ex and sy == ey:
+        return set()
+    if is_num_pad and (sx, sy) == (3, 0):
+        return set()
+    if not is_num_pad and (sx, sy) == (0, 0):
+        return set()
+    
+    dx = sx - ex
+    dy = sy - ey
+    cx = '^'
+    if dx < 0:
+        cx = 'v'
+    cy = '<'
+    if dy < 0:
+        cy = '>'
+    if dx == 0:
+        return [cy*abs(dy)]
+    if dy == 0:
+        return [cx*abs(dx)]    
+    ddx = int(dx / abs(dx))
+    ddy = int(dy / abs(dy))
+    result = set()
+    if ddx != 0:
+        result = result | set(map(lambda x: cx + x, helper(sx-ddx, sy, ex, ey, is_num_pad=is_num_pad)))
+    if ddy != 0:
+        result = result | set(map(lambda x: cy + x, helper(sx, sy-ddy, ex, ey, is_num_pad=is_num_pad)))
+    return result
+                    
+
+def translate(start, end, is_num_pad=True):
+    possible = helper(start[0], start[1], end[0], end[1], is_num_pad=is_num_pad)
+    return list(map(lambda x: x+'A', possible))
+
+def part1(codes):
+    sum = 0
+    for code in codes:        
+        sum += get_complexity(code, 2)
+    return sum
+
+def part2(codes):
+    sum = 0
+    for code in codes:        
+        sum += get_complexity(code, 25)
+    return sum
+
+def calculate_cost_map(num_robots):
+    lookup = {}
+    for i, j in itertools.permutations(DIR_PAD.keys(), 2):
+        shortest = sys.maxsize
+        for p in translate(DIR_PAD[i], DIR_PAD[j], is_num_pad=False):
+            if len(p) < shortest:
+                shortest = len(p)
+        lookup[(i, j)] = shortest
+    
+    for _ in range(1, num_robots):
+      new_lookup = {}
+      for i, j in itertools.permutations(DIR_PAD.keys(), 2):
+          shortest = sys.maxsize
+          for p in translate(DIR_PAD[i], DIR_PAD[j], is_num_pad=False):
+              cost = 0
+              for x, y in itertools.pairwise('A'+p):
+                  cost += lookup.get((x, y), 1)
+              if cost < shortest:
+                  shortest = cost
+          new_lookup[(i, j)] = shortest                
+      lookup = new_lookup
+    cost_map = {}
+    for i, j in itertools.permutations(NUM_PAD.keys(), 2):
+        shortest = sys.maxsize
+        for p in translate(NUM_PAD[i], NUM_PAD[j], is_num_pad=True):
+            cost = 0
+            for x, y in itertools.pairwise('A'+p):
+                cost += lookup.get((x, y), 1)
+            if cost < shortest:
+                shortest = cost
+        cost_map[(i, j)] = shortest 
+    return cost_map            
+    
+def get_complexity(code, num_robots):
+    cost_map = calculate_cost_map(num_robots)
+    sum = 0
+    for i, j in itertools.pairwise('A' + code):
+        sum += cost_map[(i, j)] * int(code[:-1])
+    return sum
+    
 @profiler
 def main(argv):
-  input_file = argv[0]
-  file = open(input_file, 'r')
-  for line in file.readlines():
-    print(line)
+    input_file = argv[0]
+    file = open(input_file, 'r')
+    codes = []
+    for line in file.readlines():
+        codes.append(line.strip())
+    print(part1(codes))
+    print(part2(codes))
+
+
 
 if __name__ == "__main__":
-  main(sys.argv[1:])
+    main(sys.argv[1:])
