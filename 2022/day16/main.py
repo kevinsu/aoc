0bc78b3e2a0baa07007b2f2a3b656a97b540d8a9
Kevin Su <kevinsoup@gmail.com>
2025-01-08 21:11:14 -0500

    aoc day 16 and 17 2022

--- 2022/day16/main.py
+++ 2022/day16/main.py
@@ -1,12 +1,101 @@
+import re
 import sys
 from common.perf import profiler
+import itertools
+from collections import defaultdict
 
+class Valve:
+  def __init__(self, name, rate, children):
+    self.name = name
+    self.rate = rate
+    self.children = children
+  
+  def __str__(self):
+    return f'Name: {self.name} Rate: {self.rate} Children: {self.children}'
+
+def get_key(opened):
+  return ",".join(sorted(map(lambda x: x[0], opened)))
+
+def get_pressure(valves, order, limit):
+  expected = 0
+  for i, j, _ in order:
+    expected += (limit - int(j)) * valves[i].rate
+  return expected
+
+def get_key_and_pressure(valves, order):
+  names = []
+  relief = 0
+  for name, time_remaining in order:
+    valve = valves[name]
+    names.append(name)
+    relief += (time_remaining - 1) * valve.rate
+  return ",".join(sorted(names)), relief
+
+def build_valve_mapping(valves, graph, limit):
+  mapping = defaultdict(int)
+  test = []
+  queue = [(limit, 'AA', set(), [])]
+  while queue:
+    time_remaining, name, opened, order = queue.pop()
+    key, pressure = get_key_and_pressure(valves, order)
+    mapping[key] = max(mapping[key], pressure)    
+    test.append(order)
+    for neighbor, dt in graph[name].items():
+      if neighbor in opened or neighbor == 'AA':
+        continue
+      if dt + 1 < time_remaining:
+        queue.append((time_remaining-dt-1, neighbor, opened | {neighbor}, order + [(neighbor, time_remaining-dt)]))
+  return mapping
+
+def part1(valves, graph):
+  mapping = build_valve_mapping(valves, graph, 30)
+  return max(mapping.values())
+
+def part2(valves, graph):
+  mapping = build_valve_mapping(valves, graph, 26)
+  relief = 0
+  for me, elephant in itertools.combinations(mapping.keys(), 2):
+    me_opened = set(me.split(","))
+    elephant_opened = set(elephant.split(","))
+    if me_opened.isdisjoint(elephant_opened):
+      new_relief = mapping[me] + mapping[elephant]
+      relief = max(relief, new_relief)
+  return relief
+  
+def floyd_warshall(valves):
+  dist = {}
+  for valve in valves.values():
+    dist[valve.name] = defaultdict(lambda: sys.maxsize)
+    for child in valve.children:
+      dist[valve.name][child] = 1
+  for k in valves:
+    for i in valves:
+      for j in valves:
+        dist[i][j] = min(dist[i][j], dist[i][k]+dist[k][j])
+  return dist
+  
 @profiler
 def main(argv):
   input_file = argv[0]
   file = open(input_file, 'r')
+  valves = {}
+  max_rate = 0
+  active = {'AA'}
   for line in file.readlines():
-    print(line)
-
+    m = re.match("Valve (\w*) has flow rate=(\d+); tunnel(s?) lead(s?) to valve(s?) ([\s\S]*)", line)
+    name, rate, _, _, _, children = m.groups()
+    valve = Valve(name, int(rate), list(map(lambda x: x.strip(), children.strip().split(","))))
+    valves[name] = valve
+    max_rate += valve.rate
+    if valve.rate > 0:
+      active.add(name)
+  dist = floyd_warshall(valves)
+  graph = defaultdict(dict)
+  for v1, v2 in itertools.combinations(active, 2):
+    graph[v1][v2] = dist[v1][v2]
+    graph[v2][v1] = dist[v2][v1]
+  print(part1(valves, graph))
+  print(part2(valves, graph))
+  
 if __name__ == "__main__":
   main(sys.argv[1:])
--- 2022/day17/main.py
+++ 2022/day17/main.py
@@ -1,12 +1,128 @@
+from math import floor
 import sys
 from common.perf import profiler
+from common.io import pretty_print
+from common.loop import CycleFinder
+
+ROCK_TYPES = {
+  0 : {(0, 0), (0, 1), (0, 2), (0, 3)},
+  1 : {(0, 1), (1, 0), (1, 1), (1, 2), (2, 1)},
+  2 : {(0, 0), (0, 1), (0, 2), (1, 2), (2, 2)},
+  3 : {(0, 0), (1, 0), (2, 0), (3, 0)},
+  4 : {(0, 0), (0, 1), (1, 0), (1, 1)}
+}
+
+def pp(grid):
+  pretty_print(reversed(grid))
+
+class Rock:
+  def __init__(self, x, y, rock_type):
+    self.x = x
+    self.y = y
+    self.tiles = ROCK_TYPES[rock_type]
+  
+  def get_left_tiles(self):
+    for tile in self.tiles:
+      if (tile[0], tile[1]-1) not in self.tiles:
+        yield (tile[0] + self.x, tile[1] + self.y)    
+    
+  def get_right_tiles(self):
+    for tile in self.tiles:
+      if (tile[0], tile[1]+1) not in self.tiles:
+        yield (tile[0] + self.x, tile[1] + self.y)    
+
+  def get_below_tiles(self):
+    for tile in self.tiles:
+      if (tile[0]-1, tile[1]) not in self.tiles:
+        yield (tile[0] + self.x, tile[1] + self.y)    
+    
+  def get_tiles(self):
+    return [(x+self.x, y+self.y) for x, y in self.tiles]
+
+  def blow(self, grid, direction):
+    if direction == '<':
+      for left_tile in self.get_left_tiles():
+        if left_tile[1] == 0:
+          return
+        if grid[left_tile[0]][left_tile[1]-1] != '.':
+          return
+      self.y -= 1
+    else:
+      for right_tile in self.get_right_tiles():        
+        if right_tile[1] == len(grid[0])-1:
+          return
+        if grid[right_tile[0]][right_tile[1]+1] != '.':
+          return
+      self.y += 1
+  
+  def drop(self, grid):
+    for below_tile in self.get_below_tiles():
+      if below_tile[0] == 0:
+        return False
+      if grid[below_tile[0]-1][below_tile[1]] != '.':
+        return False
+    self.x -= 1
+    return True
+
+  def settle(self, grid):
+    new_top = 0
+    for tile in self.get_tiles():
+      grid[tile[0]][tile[1]] = '#'
+      new_top = max(new_top, tile[0])
+    return new_top + 1
+    
+def get_grid_state(grid, pattern, count, pattern_index):
+  return f'{count%5} {pattern_index % len(pattern)} {"".join(["".join(row) for row in grid[-50:-6]])}'
+
+GOAL = 1000000000000
+def get_height(cycle_heights, cycle_start, cycle_height, count, current_height):
+  cycle_length = count - cycle_start + 1
+  multiplier = (GOAL - current_height) / cycle_length
+  remainder = (GOAL - current_height) % cycle_length
+  return current_height + cycle_height * multiplier + cycle_heights[remainder]
+
+# Find the first repeat and mark it as the start of the cycle.  
+# Until we see the repeat again, save the count and height
+# When we see the repeat for the second time, break.
+def part1(pattern, total_rocks):
+  grid = [['.'] * 7]
+  count = 0
+  top_of_rocks = 0
+  pattern_index = 0
+  visited = {}
+  cycle_finder = CycleFinder()
+  previous = 0
+  
+  while count < total_rocks:
+    spawn_height = top_of_rocks + 7
+    if len(grid) < spawn_height:
+      grid.extend([['.'] * 7 for i in range(spawn_height-len(grid))])      
+    grid_state = get_grid_state(grid, pattern, count, pattern_index)
+    if cycle_finder.find_cycle(grid_state, count, top_of_rocks):
+      break
+    if count % 1720 == 5:
+      print(top_of_rocks-previous, count)
+      previous = top_of_rocks
+    visited[grid_state] = count, top_of_rocks   
+    rock = Rock(top_of_rocks+3, 2, count%5)
+    can_drop = True
+    while can_drop:
+      rock.blow(grid, pattern[pattern_index % len(pattern)])
+      can_drop = rock.drop(grid)
+      pattern_index += 1
+    new_top = rock.settle(grid)
+    top_of_rocks = max(top_of_rocks, new_top)
+    count += 1    
+  print(top_of_rocks)
+  print(cycle_finder.calculate_step(GOAL))
+    
 
 @profiler
 def main(argv):
   input_file = argv[0]
   file = open(input_file, 'r')
-  for line in file.readlines():
-    print(line)
+  pattern = file.readline()  
+  print(part1(pattern, 20000))  
 
 if __name__ == "__main__":
   main(sys.argv[1:])
